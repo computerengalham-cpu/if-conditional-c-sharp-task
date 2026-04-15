32e3a61aef54bcfedfe479c3b2e1cb1cda088050
“Elham” <alham@gmail.com>
2026-04-15 11:24:33 +0400

    C# If Condition Practice Tasks

---
+++ solution4/project1/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //q1:
+            //Console.WriteLine("Please Enter Number");
+            //int num=Convert.ToInt32(Console.ReadLine());
+            //if(num>0)
+            //{ Console.WriteLine("positive number"); }
+            //else if(num<0)
+            //{ Console.WriteLine("negative number"); }
+            //else if (num==0)
+            //{ Console.WriteLine("zero"); }
+            //else { Console.WriteLine("not number"); }
+
+
+            //q2:
+            //Console.WriteLine("please enter number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if(num %2 == 0)
+            //{
+            //    Console.WriteLine(num+ " is an even number");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine(num+ " is an odd number");
+            //}
+
+
+            //q3:
+            //Console.WriteLine("please enter student score from 0 to 100");
+            //int score = Convert.ToInt32(Console.ReadLine());
+            //if (score >= 90 && score <= 100)
+            //{
+            //    Console.WriteLine("Excellent");
+            //}
+            //else if (score >= 75 )
+            //{ Console.WriteLine("very good"); }
+            //else if (score >= 60)
+            //{ Console.WriteLine("good"); }
+            //else if (score >= 50)
+            //{ Console.WriteLine("pass"); }
+            //else if (score < 50)
+            //{ Console.WriteLine("fail"); }
+            //else { Console.WriteLine("out of range"); }
+
+            //q4:
+            //Console.WriteLine("please enter username and password");
+            //string username = Console.ReadLine();
+            //string password = Console.ReadLine();
+            //if (username == "admin" && password == "1234")
+            //{
+            //    Console.WriteLine("login successful");
+            //}
+            //else { Console.WriteLine("invalid username or password"); }
+
+            //q5:
+            //double initiaL_balance = 1000;
+            //Console.WriteLine("enter a withdrawal amount");
+            //double withdrawal_amount = Convert.ToDouble(Console.ReadLine());
+            //if(withdrawal_amount<=0)
+            //{ Console.WriteLine("invalid amount"); }
+            //else if(withdrawal_amount> initiaL_balance)
+            //{ Console.WriteLine("insufficient balance"); }
+            //else
+            //{
+            //    initiaL_balance = initiaL_balance - withdrawal_amount;
+            //    Console.WriteLine("withdrawal successful");
+            //    Console.WriteLine("remaining balance:" + initiaL_balance);
+            //}
+        }
+    }
+}
